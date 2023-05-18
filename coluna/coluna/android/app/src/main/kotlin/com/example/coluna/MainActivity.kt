2c61c2447cad4957919c6606a264d5205e7ff93f
endroni <rubao.rcs@gmail.com>
2023-05-18 16:54:03 -0300

    column demonstration

---
+++ coluna/coluna/android/app/src/main/kotlin/com/example/coluna/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.coluna
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ coluna/coluna/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Text('SENAI'),
+            Text('Flutter'),
+            Expanded(
+              child: FittedBox(
+                child: FlutterLogo(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
